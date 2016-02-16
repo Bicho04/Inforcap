cc657365a586fbae10065b5c5c3be05509d724bd
Bicho04 <Bicho04@gmail.com>
2016-02-16 08:27:33 -0300

    V0.1
    Maquetación realizada para Fincreativo

---
+++ _script/functions.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.pictures').owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 4500,
+        autoplayHoverPause: true,
+        center: true,
+        loop: true,
+        margin: 0,
+        nav: false,
+        items: 1
+    });
+});
